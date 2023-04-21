26034d82cda94f426719a23f29819c9dbb058f13
Dominik Ferber <dominik.ferber@gmail.com>
2023-04-21 15:33:08 +0300

    only read edge config if the value is actually used

--- middleware.ts
+++ middleware.ts
@@ -3,22 +3,21 @@ import { NextResponse } from "next/server";
 import { get } from "@vercel/edge-config";
 
 export default async function middleware(request: Request) {
-  const blockedIps = (await get("blockedIps")) as string[];
   const ip = ipAddress(request);
-
   const url = new URL(request.url);
   const path = url.pathname;
 
-  if (path === "/am-i-blocked" && ip && blockedIps.includes(ip as string)) {
-    return new NextResponse("Access denied", {
-      status: 403,
-      headers: {
-        "Cache-Control": "no-cache",
-      },
+  if (path !== "/am-i-blocked") {
+    return next({
+      headers: { "x-your-ip-address": ip || "unknown" },
     });
   }
 
-  return next({
-    headers: { "x-your-ip-address": ip || "unknown" },
-  });
+  const blockedIps = await get<string[]>("blockedIps");
+  if (ip && blockedIps.includes(ip as string)) {
+    return new NextResponse("Access denied", {
+      status: 403,
+      headers: { "Cache-Control": "no-cache" },
+    });
+  }  
 }
