8fdd7758cdf938105666dfd62cd96f572c5d21d3
Louis <louisthomas.pro@gmail.com>
2023-04-22 20:18:41 +0800

    Refactor

--- middleware.ts
+++ middleware.ts
@@ -7,20 +7,16 @@ export default async function middleware(request: Request) {
   const url = new URL(request.url);
   const path = url.pathname;
 
-  if (path !== "/am-i-blocked") {
-    return next({
-      headers: { "x-your-ip-address": ip || "unknown" },
-    });
+  if (path === "/am-i-blocked") {
+    const blockedIps = await get<string[]>("blockedIps");
+    if (ip && blockedIps && blockedIps.includes(ip)) {
+      return new NextResponse("Access denied", {
+        status: 403,
+        headers: { "Cache-Control": "no-cache" },
+      });
+    }
   }
 
-  const blockedIps = await get<string[]>("blockedIps");
-  if (ip && blockedIps.includes(ip as string)) {
-    return new NextResponse("Access denied", {
-      status: 403,
-      headers: { "Cache-Control": "no-cache" },
-    });
-  }
-  
   return next({
     headers: { "x-your-ip-address": ip || "unknown" },
   });
